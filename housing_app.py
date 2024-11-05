e81e2ed2e631db628f3d693be1904d029b2755ac
Wathuta Wamugi <wathutaww@gmail.com>
2024-11-05 04:10:41 +0300

    Update housing_app.py

--- housing_app.py
+++ housing_app.py
@@ -130,7 +130,7 @@ st.title("California Housing Prices Prediction")
 st.markdown("""
 ##### A web application for predicting California Housing Prices.
  
-This app uses machine learning to predict the price of the house :house: :heavy_dollar_sign:. 
+<p>This app uses machine learning to predict the price of the house :house: :heavy_dollar_sign:.</p> 
 It loads a pre-trained linear regression model, which takes as input various features of the house, 
 such as the number of rooms, the number of bedrooms, the population of the house's neighborhood, and the distance to the nearest city. 
 The app preprocesses the input data by combining some of the features and adding new features, such as the distance to the nearest city.
