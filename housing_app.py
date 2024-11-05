c5b5168f9249475313019125423f8b34b864c95a
Wathuta Wamugi <wathutaww@gmail.com>
2024-11-05 03:20:14 +0300

    Update housing_app.py

--- housing_app.py
+++ housing_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-st.set_page_config(page_title="Housing Prices Prediction", page_icon=":house:")
+st.set_page_config(page_title="Housing Prices Prediction", page_icon=":building:")
 import pandas as pd
 import numpy as np
 import pickle
